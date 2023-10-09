f499f852f8d1a5aa51a89216691b0f8234f7ef2b
kinyuykelly <kinyuykelly213@gmail.com>
2023-10-09 19:00:31 +0100

    Added carousel for sponsors and partners

--- wtm-bamenda-website-frontend/src/App.js
+++ wtm-bamenda-website-frontend/src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './pages/Landing';
-
 function App() {
-  return <Landing />;
+  return <Landing/>;
 }
-
 export default App;
