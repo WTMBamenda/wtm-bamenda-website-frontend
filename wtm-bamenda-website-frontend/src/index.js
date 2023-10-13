32861b99fa1fc15ea7b0f439b77ae5acaf885373
Dabi-ayu <dabiayu8@gmail.com>
2023-10-13 03:52:43 +0100

    added card for member's profile

--- wtm-bamenda-website-frontend/src/index.js
+++ wtm-bamenda-website-frontend/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import MemberProfile from './pages/MemberProfile';
+import memberImage1 from "./assets/home/s3-img-1.JPG";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let memberInfo = {
+  image: memberImage1,
+  twitterProfile: `https://twitter.com/AmandineNangah/status/1358828632924430336`,
+  githubProfile: `https://github.com/nangahamandine`,
+  linkedinProfile:`Linkedin: https://www.linkedin.com/in/nangah-amandine`
+}
 root.render(
   <React.StrictMode>
     <App />
+    <MemberProfile info={memberInfo} />
   </React.StrictMode>
 );
 
