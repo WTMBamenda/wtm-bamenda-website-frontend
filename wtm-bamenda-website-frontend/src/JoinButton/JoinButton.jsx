138abf3d09b3f57e2ffdaa16fd16c5051ed11088
Ndzedzenyuy Nadine <ndzedzenyuynadine@gmail.com>
2023-10-19 03:35:23 +0100

    Added NavBar component andthe JoinButton component

---
+++ wtm-bamenda-website-frontend/src/JoinButton/JoinButton.jsx
@@ -0,0 +1,12 @@
+import './JoinButton.css'
+
+function JoinButton(){
+    return(
+        <div>
+       <button className="join-button">Join Our Community</button> 
+       </div>
+    )
+}
+
+
+export default JoinButton;
