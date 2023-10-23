5fe36e288523bc56d7ce6ac01238285ca119e1b0
kinyuykelly <kinyuykelly@gmail.com>
2023-10-23 14:13:31 +0100

    Added bottom navigation

--- wtm-bamenda-website-frontend/src/App.js
+++ wtm-bamenda-website-frontend/src/App.js
@@ -1,12 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './pages/Landing';
-import Footer from './Footer/Footer';
 function App() {
-<<<<<<< HEAD
-  return <Footer/>;
-=======
   return <Landing/>
->>>>>>> 859a174 (Added bottom navigation)
 }
 export default App;
